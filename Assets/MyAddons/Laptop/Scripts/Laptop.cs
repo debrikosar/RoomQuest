ec5791f74da3d01673ef3ceef7ac4211218c0823
Nikita Pavlov <normandbear@gmail.com>
2021-04-16 12:08:35 +0300

    Update_0.0.3
    + Laptop (Animation, UI)

---
+++ Assets/MyAddons/Laptop/Scripts/Laptop.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Laptop : MonoBehaviour, IInteract
+{
+    [SerializeField]
+    private PlayerControl playerControl;
+
+    [SerializeField]
+    private Camera gameCamera;
+
+    [SerializeField]
+    private GameObject targetPosition;
+
+    private bool isCameraZoom;
+    private float speed = 3f;
+    private GameObject cameraTransformData;
+    private Coroutine startedCoroutine;
+
+    public void Start()
+    {
+        cameraTransformData = new GameObject();
+    }
+
+    public void ToInteract()
+    {
+        if (!isCameraZoom)
+        {
+            startedCoroutine = StartCoroutine(ZoomCoroutine(targetPosition.transform));
+        }
+        else
+        {
+            startedCoroutine = StartCoroutine(ZoomCoroutine(cameraTransformData.transform));
+        }
+        SwitchPlayerControl();
+    }
+
+    public IEnumerator ZoomCoroutine(Transform ZoomTo)
+    {
+        if (!isCameraZoom)
+        {
+            SaveCameraPosition();
+            speed = 3;
+        }
+        else
+            speed = 7;
+            
+
+        isCameraZoom = !isCameraZoom;
+
+        while (Vector3.Distance(gameCamera.transform.position, ZoomTo.position) >= 0.1f || Quaternion.Angle(gameCamera.transform.rotation, ZoomTo.rotation) >= 0.1f)
+        {
+            gameCamera.transform.position = Vector3.Lerp(gameCamera.transform.position, ZoomTo.position, speed * Time.deltaTime);
+            gameCamera.transform.rotation = Quaternion.Lerp(gameCamera.transform.rotation, ZoomTo.rotation, speed * Time.deltaTime);
+
+            Debug.Log(gameCamera.transform.position + "|||" + ZoomTo.transform.position);
+
+            yield return new WaitForEndOfFrame();          
+        }
+        StopCoroutine(startedCoroutine);
+
+        //while (gameCamera.transform.position.normalized != ZoomTo.position.normalized || gameCamera.transform.rotation.normalized != ZoomTo.rotation.normalized)
+        //{
+        //    gameCamera.transform.position = Vector3.Lerp(gameCamera.transform.position, ZoomTo.position, speed * Time.deltaTime);
+        //    gameCamera.transform.rotation = Quaternion.Lerp(gameCamera.transform.rotation, ZoomTo.rotation, speed * Time.deltaTime);
+
+        //    Debug.Log(gameCamera.transform.position + "|||" + ZoomTo.transform.position);
+
+        //    yield return new WaitForEndOfFrame();
+        //}
+    }
+
+    public void SaveCameraPosition()
+    {
+        cameraTransformData.transform.position = gameCamera.transform.position;
+        cameraTransformData.transform.rotation = gameCamera.transform.rotation;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && isCameraZoom)
+        {
+            ToInteract();
+        }
+    }
+
+    public void SwitchPlayerControl()
+    {
+        playerControl.enabled = !playerControl.enabled;
+        Cursor.visible = !Cursor.visible;
+
+        if (Cursor.lockState == CursorLockMode.Locked)
+            Cursor.lockState = CursorLockMode.None;
+        else
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void ShowHint()
+    {
+        throw new System.NotImplementedException();
+    }
+}
