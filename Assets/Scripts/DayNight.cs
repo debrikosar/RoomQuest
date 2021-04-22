3b04369bfc942f063a40f641ecfe0ca33530f051
Nikita Pavlov <normandbear@gmail.com>
2021-04-22 18:46:28 +0300

    Update 0.0.10

--- Assets/Scripts/DayNight.cs
+++ Assets/Scripts/DayNight.cs
@@ -5,19 +5,11 @@ using System.Collections;
 public class DayNight : MonoBehaviour
 {
 	public Transform directionalLight;
-	//public float fullDay = 120f;
-	/*[Range(0, 1)]*/ public float currentTime;
+    [Range(0, 1)] public float currentTime;
 
 	void Update()
 	{
-        //currentTime += Time.deltaTime / fullDay;
-
-        //currentTime = 1 / 86400 * (GameTimeSystem.instance.hour * 3600 + GameTimeSystem.instance.minutes * 60 + GameTimeSystem.instance.seconds);
-
         currentTime = GameTimeSystem.instance.GetTimeForSun();
-
-        //if (currentTime >= 1) currentTime = 0; else if (currentTime < 0) currentTime = 0;
-
-        directionalLight.localRotation = Quaternion.Euler((currentTime * 360f) - 90, 170, 0);
+        directionalLight.localRotation = Quaternion.Euler((currentTime * 360f) - 90f, 195.993f, -146.348f);
 	}
 }
